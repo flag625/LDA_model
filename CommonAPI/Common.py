434f2b0faaab940047456e15bd9eb5c1152e8ca8
flag625 <liangzhisen@outlook.com>
2019-03-11 10:54:47 +0800

    设置配置文件

--- CommonAPI/Common.py
+++ CommonAPI/Common.py
@@ -6,3 +6,11 @@ import configparser
 
 conf = configparser.ConfigParser()
 conf.read("../CommonAPI/comm.conf")
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+fh = logging.FileHandler(conf.get('logging','log_path'), mode='a')
+fh.setLevel(logging.DEBUG)
+formatter = logging.Formatter("%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s")
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+print('这里是测试内容：{}'.format(conf.get('logging','log_path')))
