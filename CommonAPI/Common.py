9977f2061a4f31c051ae9553b67f69a874efef00
flag625 <liangzhisen@outlook.com>
2019-03-11 15:01:24 +0800

    词性标记

--- CommonAPI/Common.py
+++ CommonAPI/Common.py
@@ -4,6 +4,7 @@ import pandas as pd
 import json
 import os
 import string
+import re
 from collections import Counter
 import nltk
 import logging.config
@@ -38,12 +39,58 @@ class Excel2pd(object):
         return df
 
 class Segement(object):
-    def __init__(self, regEx, article):
-        self.regEx = regEx
-        self.article = article
+    # 按照规则一分段，NOVELTY作为技术词来源，USE和ADVANTAGE作为功效词来源
+    def __init__(self, regEx, text):
+        '''
+        :param regEx: 分段正则式
+        :param text: 被分段的文本
+        '''
+        self.regEx = re.compile(regEx)
+        self.text = text
 
+    def segement(self):
+        '''
+        分段
+        :return: list
+        '''
+        try:
+            listOfTokens = self.regEx.split(self.text)
+        except Exception as e:
+            logger.info(u"失败原因：")
+            logger.info(e)
+            raise e
+
+        return listOfTokens
+
+class PosTag(object):
+    def __init__(self, doc):
+        self.doc = doc
+
+    # 词性标记
+    def preprocess(self):
+        sentences = nltk.sent_tokenize(self.doc)
+        sentences = [nltk.word_tokenize(sent) for sent in sentences]
+        sentences = [nltk.pos_tag(sent) for sent in sentences]
+        return sentences
+
+    # 按照语法分块
+    def chunking(self, grammar, sentence):
+        '''
+        :param grammar:  名词短语的语法规则
+        :return:
+        '''
+        cp = nltk.RegexpParser(grammar)
+        res = cp.parse(sentence)
+        return res
 
 
 if __name__ == '__main__':
-    test_pd = Excel2pd().excel2pd()
-    print(test_pd)
+    #test_pd = Excel2pd().excel2pd()
+    #print(test_pd)
+
+    doc = u" Therefore, the invention considers the chain circuit attenuation and the effects of interference and descending load of region. " \
+          u"Compared with traditional method based on chain circuit rate attenuation, " \
+          u"the invention approaches to mobile station distribution method of the real WCDMA (wideband code division multiple access) system. "
+    pos_test = PosTag(doc)
+    sentence = pos_test.preprocess()
+    print(sentence)
